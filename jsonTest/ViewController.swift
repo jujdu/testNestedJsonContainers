41d801aa692a50d12f0500dabe3a85b94e246296
jujdu <sidoruk.misha@gmail.com>
2019-12-20 09:13:42 +0300

    Initial Commit

---
+++ jsonTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonTest
+//
+//  Created by Michael Sidoruk on 20.12.2019.
+//  Copyright © 2019 Michael Sidoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
