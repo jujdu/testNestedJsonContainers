95fbc0fac7b8c397c3bc3bb84910d0b6fd29e6ca
jujdu <sidoruk.misha@gmail.com>
2019-12-23 09:05:18 +0300

    fix with top lvl array with only item

--- jsonTest/Model.swift
+++ jsonTest/Model.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 //MARK: - первоначальная неудобная модель
-struct Welcome: Codable {
-    let feed: Feed1
+struct Welcome1: Codable {
+    let feed: [Feed1]
 }
 
 struct Feed1: Codable {
@@ -43,22 +43,29 @@ extension Feed {
     
     init(from decoder: Decoder) throws {
         //нужно проверить как декодировать если первый верхний элемент в виде массива с одним элементом
-        //MARK: - из верхнего во вложенный уровень.
-        let values = try decoder.container(keyedBy: ModelKeys.self)
-        let feed = try values.nestedContainer(keyedBy: FeedKeys.self, forKey: .feed)
-        
-        self.id = try feed.decode(Int.self, forKey: .id)
-        self.name = try feed.decode(String.self, forKey: .name)
+        //MARK: - top lvl container [Feed]
+        let container = try decoder.container(keyedBy: ModelKeys.self)
+        //MARK: - пробираемся к первому элементу массива. feed[0]
+        var feedContainer = try container.nestedUnkeyedContainer(forKey: .feed)
+        //MARK: - переводим из topLvl в Feed. Из верхнего уровня в нижний
+        let firstFeed = try feedContainer.nestedContainer(keyedBy: FeedKeys.self)
+        self.id = try firstFeed.decode(Int.self, forKey: .id)
+        self.name = try firstFeed.decode(String.self, forKey: .name)
         
         //MARK: -  из вложенного на верхний уровень
-        let company = try feed.nestedContainer(keyedBy: CompanyKeys.self, forKey: .company)
+        let company = try firstFeed.nestedContainer(keyedBy: CompanyKeys.self, forKey: .company)
         let companyName = try company.decode(String.self, forKey: .name)
         let shortCompanyName = try company.decode(String.self, forKey: .shortName)
         
         self.companyName = companyName + " " + shortCompanyName
     }
 }
-
+//a good answer with description and also a cool thread at all. Saved it.
+//https://stackoverflow.com/a/44549825/10106098
+//nestedContainer(keyedBy:forKey:) to get a nested object from an object for a given key
+//nestedUnkeyedContainer(forKey:) to get a nested array from an object for a given key
+//nestedContainer(keyedBy:) to get the next nested object from an array
+//nestedUnkeyedContainer() to get the next nested array from an array
 
 
 
